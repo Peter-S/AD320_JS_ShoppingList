466600ff264dbe0e33324cee0ecbaf43e9ba9966
leitto10 <46364179+leitto10@users.noreply.github.com>
2019-03-06 13:39:41 -0800

    get and post callbacks

--- routes/shoppingList.js
+++ routes/shoppingList.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('shoppingList', { 
       title: 'Shopping List' ,
-      newTask: req.query.newTask,
-      quantity: req.query.quantity,
+        });
+});
+
+router.post('/', function(req, res, next) {
+    console.dir(req.body);
+  res.render('shoppingList', {
+      name:req.body.username,
+      title: 'Shopping List' ,
       
         });
 });
